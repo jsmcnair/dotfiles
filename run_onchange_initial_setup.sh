515186c835103f2aec534ca656c0125d0fc64d10
jsmcnair <john.mcnair@yellowdog.co>
2024-02-19 11:38:50 +0000

    Update dotfiles

--- run_onchange_initial_setup.sh
+++ run_onchange_initial_setup.sh
@@ -1,18 +1,48 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm --needed \
-  swayidle
+# Locale
+LOCALE="en_GB.UTF-8"
+sudo sed -i -E "s|^#($LOCALE)|\1|" /etc/locale.gen
+sudo locale-gen
+echo "LANG=$LOCALE" | sudo tee /etc/locale.conf > /dev/null
 
+# Timezone
+TIMEZONE="Europe/London"
+sudo ln -sf "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime
+
+# Keyboard layout
+KEYS="uk"
+sudo loadkeys $KEYS
+echo "KEYMAP=$KEYS" | sudo tee /etc/vconsole.conf > /dev/null
+
+# Basics
 sudo pacman -S --noconfirm --needed \
-  zsh github-cli otf-droid-nerd bitwarden-cli neovim \
-  firefox firefox-i18n-en-gb man-db
+  zsh git github-cli otf-droid-nerd bitwarden-cli neovim \
+  firefox firefox-i18n-en-gb man-db starship base-devel \
+  htop
+
+# Desktop
+if [[ ! -e ~/.config/hypr/hyprland.conf ]]; then
+  git clone https://github.com/jsmcnair/hyprdots ~/Hyprdots
+  cd ~/Hyprdots/Scripts
+  ./install.sh
+fi
 
 ## Lunarvim
 sudo pacman -S --noconfirm --needed make gcc python python-pip npm rustup
 [[ ! -e ~/.local/bin/lvim ]] && LV_BRANCH='release-1.3/neovim-0.9' curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh | bash -
 
-# Starship
-curl -S https://starship.rs/install.sh | sudo sh
+# SSH
+if [[ ! -e ~/.ssh/id_ed25519 ]]; then
+  echo ""
+  echo "SSH key does not exist, creating..."
+  echo ""
+  ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519
+fi
 
-## SSH
-[[ ! -e ~/.ssh/id_ed25519 ]] && ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519
+# Bitwarden Login
+if [[ ! -e ~/.config/Bitwarden\ CLI/data.json ]]; then
+  echo ""
+  echo "Running bitwarden login..."
+  echo ""
+fi
